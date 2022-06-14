83d9299f61bd9008575ddc76462f35a15ce52c18
aaakch0316 <aaakch0316@gmail.com>
2022-06-14 15:23:07 +0900

    [!] change crossorigin to crossOrigin

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="icon" href="/favicon.ico" />
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6098361082126605" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6098361082126605" crossOrigin="anonymous"></script>
 
       </Head>
       <Layout>
